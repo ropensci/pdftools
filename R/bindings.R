d88059fe85a8dfa7f1d8c884813b3777856fe213
Jeroen Ooms <jeroenooms@gmail.com>
2016-02-24 02:21:44 +0100

    Pass CMD check

--- R/bindings.R
+++ R/bindings.R
@@ -1,13 +1,19 @@
 #' Bindings to poppler
 #'
-#' Bla bal
+#' Extract text or data from PDF files.
 #'
 #' @export
+#' @param pdf file path or raw vector with pdf data
+#' @param owner_password string with owner password to open pdf
+#' @param user_password string with user password to open pdf
 #' @useDynLib poppler
 #' @rdname poppler
-#' @aliases poppler
+#' @aliases poppler poppler_version
 #' @importFrom Rcpp sourceCpp
 #' @export poppler_version
+#' @examples pdffile <- "https://cran.r-project.org/doc/manuals/r-release/R-exts.pdf"
+#' download.file(pdffile, "R-exts.pdf", mode = "wb")
+#' pdf_info("R-exts.pdf")
 pdf_info <- function(pdf, owner_password = "", user_password = "") {
   if(is.character(pdf)){
     path <- normalizePath(pdf, mustWork = TRUE)
