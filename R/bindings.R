9c50e5a889ba0d995cc9f85b47f92250b7b4ccc6
Jeroen Ooms <jeroenooms@gmail.com>
2016-02-24 16:12:37 +0100

    Documentation tweaks

--- R/bindings.R
+++ R/bindings.R
@@ -11,7 +11,7 @@
 #' @aliases poppler poppler_version
 #' @importFrom Rcpp sourceCpp
 #' @export poppler_version
-#' @examples pdffile <- "https://cran.r-project.org/doc/manuals/r-release/R-exts.pdf"
+#' @examples pdffile <- "http://cran.r-project.org/doc/manuals/r-release/R-exts.pdf"
 #' download.file(pdffile, "R-exts.pdf", mode = "wb")
 #' pdf_info("R-exts.pdf")
 #' pdf_text("R-exts.pdf")
@@ -27,8 +27,23 @@ pdf_text <- function(pdf, owner_password = "", user_password = "") {
 
 loadfile <- function(pdf){
   if(is.character(pdf)){
-    path <- normalizePath(pdf, mustWork = TRUE)
-    pdf <- readBin(path, raw(), file.info(path)$size)
+    if(grepl("^https?://", pdf[1])){
+      pdf <- url(pdf)
+    } else {
+      path <- normalizePath(pdf, mustWork = TRUE)
+      pdf <- readBin(path, raw(), file.info(path)$size)
+    }
+  }
+  if(inherits(pdf, "connection")){
+    con <- pdf
+    pdf <- raw()
+    if(!isOpen(con)){
+      open(con, "rb")
+      on.exit(close(con))
+    }
+    while(length(buf <- readBin(con, raw(), 1e6))){
+      pdf <- c(pdf, buf)
+    }
   }
   if(!is.raw(pdf))
     stop("Argument pdf must be a path or raw vector with PDF data")
