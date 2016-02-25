0060f146675820a6edf9b107b8f5ef0ed1220840
Jeroen Ooms <jeroenooms@gmail.com>
2016-02-25 21:55:50 +0100

    Set a custom error callback

---
+++ src/init.cpp
@@ -0,0 +1,20 @@
+#include <Rcpp.h>
+#include <R_ext/Rdynload.h>
+#include <poppler-global.h>
+#include <poppler-version.h>
+
+#if defined(POPPLER_VERSION_MINOR) && POPPLER_VERSION_MINOR >= 30
+
+/* export to C */
+extern "C" void R_init_pdftools(DllInfo *info);
+
+void error_callback(const std::string &msg, void *context){
+  Rcpp::Function err_cb = Rcpp::Environment::namespace_env("base")["message"];
+  err_cb(Rcpp::String(msg));
+}
+
+void R_init_pdftools(DllInfo *info) {
+  poppler::set_debug_error_function(error_callback, NULL);
+}
+
+#endif
