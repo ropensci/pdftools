55d84cb43f3921d5ece570b7101931489228468a
Jeroen Ooms <jeroenooms@gmail.com>
2016-03-02 22:06:12 +0100

    Switch to local example

--- R/tools.R
+++ R/tools.R
@@ -12,11 +12,12 @@
 #' @aliases pdftools
 #' @importFrom Rcpp sourceCpp
 #' @family pdftools
-#' @examples download.file("http://arxiv.org/pdf/1403.2805.pdf", "1403.2805.pdf", mode = "wb")
-#' info <- pdf_info("1403.2805.pdf")
-#' text <- pdf_text("1403.2805.pdf")
-#' fonts <- pdf_fonts("1403.2805.pdf")
-#' files <- pdf_attachments("1403.2805.pdf")
+#' @examples # Just a random pdf file
+#' file.copy(file.path(Sys.getenv("R_DOC_DIR"), "NEWS.pdf"), "news.pdf")
+#' info <- pdf_info("news.pdf")
+#' text <- pdf_text("news.pdf")
+#' fonts <- pdf_fonts("news.pdf")
+#' files <- pdf_attachments("news.pdf")
 pdf_info <- function(pdf, opw = "", upw = "") {
   poppler_pdf_info(loadfile(pdf), opw, upw)
 }
