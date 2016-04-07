d39722edb7e150ec113b4ccffd65fc4af63a1c31
Jeroen Ooms <jeroenooms@gmail.com>
2016-04-07 23:00:32 +0200

    Updates for toolchain

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,7 +1,7 @@
-# Build against mingw-w64 build of poppler 0.41.0
-if(!file.exists("../windows/poppler-0.41.0/include/poppler/cpp/poppler-document.h")){
+# Build against mingw-w64 build of poppler 0.42.0
+if(!file.exists("../windows/poppler-0.42.0/include/poppler/cpp/poppler-document.h")){
   if(getRversion() < "3.3.0") setInternet2()
-  download.file("https://github.com/rwinlib/poppler/archive/v0.41.0.zip", "lib.zip", quiet = TRUE)
+  download.file("https://github.com/rwinlib/poppler/archive/v0.42.0.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
