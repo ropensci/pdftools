0ef7bc1b8f059435afcdcfa53d633b6b3dbfc1c7
Jeroen Ooms <jeroenooms@gmail.com>
2017-06-12 12:01:16 +0200

    Fix unit tests for old poppler

--- tests/testthat/test-reading.R
+++ tests/testthat/test-reading.R
@@ -2,12 +2,17 @@ context("reading pdf files")
 
 test_that("reading password protected pdf", {
   # Read with password
-  expect_warning(expect_true(pdf_info("pdf-example-password.original.pdf")$locked))
-  expect_false(pdf_info("pdf-example-password.original.pdf", upw = "test")$locked)
+  if(poppler_config()$version >= "0.30"){
+    expect_warning(expect_true(pdf_info("pdf-example-password.original.pdf")$locked))
+    expect_error(expect_warning(pdf_text("pdf-example-password.original.pdf"), "locked"))
+  } else{
+    expect_true(pdf_info("pdf-example-password.original.pdf")$locked)
+    expect_error(pdf_text("pdf-example-password.original.pdf"), "locked")
+  }
 
   # Get text with password
   expect_equal(4, length(pdf_text("pdf-example-password.original.pdf", upw = "test")))
-  expect_error(expect_warning(pdf_text("pdf-example-password.original.pdf"), "locked"))
+  expect_false(pdf_info("pdf-example-password.original.pdf", upw = "test")$locked)
 
   # Reading 'encrypted' file
   info <- pdf_info("pdf-example-encryption.original.pdf")
