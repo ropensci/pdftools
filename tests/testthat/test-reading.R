e9b63af795d3a528bcc111bc04f530fe5f549f60
Jeroen Ooms <jeroenooms@gmail.com>
2017-06-12 11:08:27 +0200

    Add encrypted example

--- tests/testthat/test-reading.R
+++ tests/testthat/test-reading.R
@@ -8,4 +8,9 @@ test_that("reading password protected pdf", {
   # Get text with password
   expect_equal(4, length(pdf_text("pdf-example-password.original.pdf", upw = "test")))
   expect_error(expect_warning(pdf_text("pdf-example-password.original.pdf"), "locked"))
+
+  # Reading 'encrypted' file
+  info <- pdf_info("pdf-example-encryption.original.pdf")
+  expect_true(info$encrypted)
+  expect_false(info$locked)
 })
