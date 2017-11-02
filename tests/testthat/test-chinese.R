6463687601f4a7903fb3d4f436ade05e584af6fa
Jeroen <jeroenooms@gmail.com>
2017-11-02 15:36:41 +0100

    Add unit test for Chinese text

---
+++ tests/testthat/test-chinese.R
@@ -0,0 +1,9 @@
+context("use non-latin text (requires poppler-data)")
+
+test_that("reading Chinese text", {
+  suppressMessages(text <- pdf_text("chinese.pdf"))
+  expect_length(text, 1)
+  expect_equal(Encoding(text), "UTF-8")
+  expect_true(grepl("\u98A8\u96AA\u56E0\u7D20", text))
+  cat(text)
+})
