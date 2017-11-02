72481f85e1190f049770fd4c5d59e57591a1c675
Jeroen <jeroenooms@gmail.com>
2017-11-02 16:51:52 +0100

    Add more unit test about poppler-data

--- tests/testthat/test-chinese.R
+++ tests/testthat/test-chinese.R
@@ -5,4 +5,11 @@ test_that("reading Chinese text", {
   expect_length(text, 1)
   expect_equal(Encoding(text), "UTF-8")
   expect_true(grepl("\u98A8\u96AA\u56E0\u7D20", text))
+
+  # Check fonts
+  fonts <- pdf_fonts("chinese.pdf")
+  expect_equal(nrow(fonts), 3)
+  expect_true(all(fonts$embedded))
+  expect_match(fonts$name[2], "MSung")
+  expect_match(fonts$name[3], "MHei")
 })
