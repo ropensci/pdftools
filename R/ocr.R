fcb0ddb0d5bb1289faf8872f8acd0b36ee0a8144
Jeroen Ooms <jeroenooms@gmail.com>
2023-05-26 21:56:54 +0200

    Pass down tesseract options parameter Closes #125 Closes #121

--- R/ocr.R
+++ R/ocr.R
@@ -8,10 +8,11 @@
 #' @inheritParams pdftools
 #' @param language passed to [tesseract][tesseract::tesseract] to specify the
 #' languge of the engine.
+#' @param options passed to [tesseract][tesseract::tesseract] to specify OCR parameters
 #' @param pages which pages of the pdf file to extract
-#' @param dpi resolution to render image that is passed to [tesseract::ocr].
-pdf_ocr_text <- function(pdf, pages = NULL, opw = "", upw = "", language = "eng", dpi = 600){
-  engine <- tesseract::tesseract(language)
+#' @param dpi resolution to render image that is passed to [pdf_convert].
+pdf_ocr_text <- function(pdf, pages = NULL, opw = "", upw = "", dpi = 600, language = "eng", options = NULL){
+  engine <- tesseract::tesseract(language, options = options)
   images <- pdf_convert(pdf = pdf, pages = pages, opw = opw, upw = upw, dpi = dpi)
   on.exit(unlink(images))
   vapply(images, tesseract::ocr, character(1), engine = engine, USE.NAMES = FALSE)
@@ -19,7 +20,7 @@ pdf_ocr_text <- function(pdf, pages = NULL, opw = "", upw = "", language = "eng"
 
 #' @export
 #' @rdname pdf_ocr
-pdf_ocr_data <- function(pdf, pages = NULL, opw = "", upw = "", language = "eng", dpi = 600){
+pdf_ocr_data <- function(pdf, pages = NULL, opw = "", upw = "", dpi = 600, language = "eng", options = NULL){
   engine <- tesseract::tesseract(language)
   images <- pdf_convert(pdf = pdf, pages = pages, opw = opw, upw = upw, dpi = dpi)
   on.exit(unlink(images))
