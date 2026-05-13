75f7912675b9b28e1bc1dc8f8e9d7d163450700e
Jeroen Ooms <jeroenooms@gmail.com>
2026-05-13 21:06:58 +0200

    Updating poppler fallback lib

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,9 +1,11 @@
 if(!file.exists("../windows/poppler/include/poppler/cpp/poppler-document.h")){
   unlink("../windows", recursive = TRUE)
   url <- if(grepl("aarch", R.version$platform)){
-    "https://github.com/r-windows/bundles/releases/download/poppler-23.08.0/poppler-23.08.0-clang-aarch64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/poppler-25.09.1/poppler-25.09.1-clang-aarch64.tar.xz"
   } else if(grepl("clang", Sys.getenv('R_COMPILED_BY'))){
-    "https://github.com/r-windows/bundles/releases/download/poppler-23.08.0/poppler-23.08.0-clang-x86_64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/poppler-25.09.1/poppler-25.09.1-clang-x86_64.tar.xz"
+  } else if(getRversion() >= "4.6") {
+    "https://github.com/r-windows/bundles/releases/download/poppler-25.09.1/poppler-25.09.1-ucrt-x86_64.tar.xz"
   } else if(getRversion() >= "4.3") {
     "https://github.com/r-windows/bundles/releases/download/poppler-23.08.0/poppler-23.08.0-ucrt-x86_64.tar.xz"
   } else {
